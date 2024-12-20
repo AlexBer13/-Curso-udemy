07e7237f6cc71eb19ff9a4f4f864c7f6bbd07501
Alexis Gastelum Bernal <gokku276@gmail.com>
2024-12-20 10:28:38 -0600

    error 404

---
+++ admin_clinica/src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SharedModule } from './shared/shared.module';
+import { MAT_DATE_LOCALE } from '@angular/material/core';
+
+
+
+
+// import { HeaderComponent } from './common-component/header/header.component';
+// import { SidebarComponent } from './common-component/sidebar/sidebar.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    // HeaderComponent,
+    // SidebarComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    SharedModule
+  ],
+  providers: [{provide: MAT_DATE_LOCALE, useValue: 'es-ES'}],
+  bootstrap: [AppComponent],
+  exports: [
+    // HeaderComponent,
+    // SidebarComponent,
+  ]
+})
+export class AppModule { }
